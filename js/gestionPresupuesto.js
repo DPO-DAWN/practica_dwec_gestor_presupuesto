429892ff049ab5a9b7dbd1e44a1bb0c6598b8b5b
David Pérez Olivas <perez21299@iesmarenostrum.com>
2021-10-04 19:02:32 +0200

    2021/04/10

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -1,18 +1,30 @@
 // TODO: Crear las funciones, objetos y variables indicadas en el enunciado
 
 // TODO: Variable global
-let presupuesto = 0;
+var presupuesto = 0;
 
-function actualizarPresupuesto() {
+function actualizarPresupuesto(valorPresupuesto) {
     // TODO
+    if(presupuesto<0)
+    alert('Has introducido un valor erróneo. \n -1');
+    else
+    presupuesto=valorPresupuesto;
+    alert(presupuesto);
 }
 
 function mostrarPresupuesto() {
     // TODO
+    alert(`Tu presupuesto actual es de ${presupuesto}€`);
 }
 
 function CrearGasto() {
     // TODO
+    this.descripcion= descripcion;
+    this.valor= valor;
+    if(valor<0)
+    alert('El valor es erróneo');
+    else
+    valor=valor;
 }
 
 // NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
--- test/testIntroJSI.js
+++ test/testIntroJSI.js
@@ -1,5 +1,5 @@
 // Importar librería Chai para realizar aserciones
-import {assert} from "chai";
+import {assert} from "Chai";
 
 // Importar código de la aplicación sobre la que se realizarán los tests
 import {actualizarPresupuesto, mostrarPresupuesto, CrearGasto} from '../js/gestionPresupuesto.js';
